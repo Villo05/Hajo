6de9b27150675b99887e58207a4da1828b18d6a1
Villo05 <nemethvillo.2005@gmail.com>
2026-09-11 12:26:46 +0200

    kosarba esemeny elkezdese

--- VIEW/HajoTeljes.js
+++ VIEW/HajoTeljes.js
@@ -5,18 +5,30 @@ export default class HajoTeljes{
         this.#obj=obj;
         this.#szuloElem=szuloElem;
         this.megjelenit();
+        this.buttonElem=document.querySelector(".hajoTeljes:last-child button")
+        console.log(this.buttonElem)
+        this.esemenykezelo()
     }
 
     megjelenit(){
         const SZOVEG=`
-            <div>
+            <div class="hajoTeljes">
                 <h2>${this.#obj.nev}</h2>
                 <p><span>Kapitány neve: </span>${this.#obj.kapitany}</p>
                 <p><span>Hajó színe: </span>${this.#obj.szin}</p>
                 <p><span>Valódi e: </span>${this.#obj.valodi}</p>
                 <p><span>Hajó leírása: </span>${this.#obj.leiras}</p>
+                <button> Kosár </button>
             </div>
         `;
         this.#szuloElem.insertAdjacentHTML("beforeend",SZOVEG)
+    }
+
+    esemenykezelo(){
+        this.buttonElem.addEventListener("click", ()=> {
+            const e = new CustomEvent("kosarba", {detail:this.#obj.id})
+            window.dispatchEvent(e);
+            console.log(this.#obj.id)
+        })
     }
 }
